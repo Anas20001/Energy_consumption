1c8df2b44c4b2844cd38880a5eedcaa03920b894
Anas Mohamed <anasmohemmed21@yahoo.com>
2023-08-10 08:13:12 +0000

    Refactor Analytics to be Modular
    - Broke down analytics functions into smaller, reusable modules.
    - Improve code readability and maintainability by separating concerns.
    - This change will allow easier future enhancements.

---
+++ README.md
@@ -0,0 +1,39 @@
+# Energy Consumption Data Analysis Repository
+
+This repository contains resources and tools for analyzing energy consumption datasets.
+
+## 📄 Overview
+
+- `Data_analysis_tasks_with_python.pdf`: A guide to tasks associated with this project.
+- `electricity_consumption.ipynb`: Jupyter Notebook for visualizing and analyzing electricity consumption data.
+- `energy_analysis.py`: Python script for energy data processing.
+- `daily_lp_electricity_consumption.png`: A visual representation of daily load profile electricity consumption.
+- `requirements.txt`: Required libraries and dependencies for running the project.
+
+## 📁 Directory Structure
+
+- `data`: Contains the datasets used for the project.
+  - `preprocessed`: Contains preprocessed data files.
+    - `lp.csv`: Load profile data in CSV format.
+    - `lp_table.csv`: Table representation of the load profile data.
+  - `raw`: Contains the raw datasets.
+- `plots`: Contains visualizations derived from the data.
+  - `AT`: (Contents to be detailed as per requirement.)
+  - `lp`: Visualizations related to load profile.
+    - Monthly breakdown visualizations for energy consumption for each month of 2019.
+    - Summarized visualizations: daily, monthly, weekly, and yearly energy consumptions.
+  - `profil`: (Contents to be detailed as per requirement.)
+
+## 🛠 Getting Started
+
+1. Clone the repository:
+        - git clone <repo_link>
+
+2. Navigate to the project directory:
+        - cd energy_consumption
+
+3. Install the required packages:
+        - pip install -r requirements.txt
+
+4. Run the analysis script or open the Jupyter notebook to view visualizations. 
+
--- energy_analysis.py
+++ energy_analysis.py
@@ -7,9 +7,14 @@ class clr:
     E = '\033[0m'
     
 def create_features(df):
-    
     """
     Create time series features based on time series index.
+    
+    Args:
+    - df (pd.DataFrame): dataframe with a datetime index.
+    
+    Returns:
+    - pd.DataFrame: Dataframe with additional time-related features.
     """
     df = df.copy()
     df['hour'] = df.index.hour
@@ -21,69 +26,162 @@ def create_features(df):
     
     return df
 
-def compute_analysis(dataset_path):
+def display_results_table(results_df):
+    """
+    Display a formatted results table.
     
-    df = pd.read_csv(dataset_path, n_rows=10)
-    print(df)
+    Args:
+    - results_df (pd.DataFrame): Input dataframe containing results.
+    """
     
-    skip_rows = int(input("Enter the number of rows to skip: "))
-    df = pd.read_csv(dataset_path, skiprows=skip_rows, 
-                     names = ['date', 'energy_consumption'], header=0,
-                     parse_dates=['date'], index_col='date')
-    
-    unit = input("Do you want the results in kWh or kW? ").strip().lower()
-    if unit == 'kw':
-        df['energy_consumption'] = df['energy_consumption'] / df.index.to_series().diff().dt.total_seconds() * 3600
-    
-    df = create_features(df)
-    
-    results = {
-        "Name of dataset": [dataset_path.split('/')[-1]],
-        "Time period of dataset": [f"{df.index.min().strftime('%d.%m.%Y')}-{df.index.max().strftime('%d.%m.%Y')}"],
-        "Count amount of kWh values": [len(df)],
-        "Total energy consumption": [df['energy_consumption'].sum()],
-        "Maximum value in dataset": [f"{df['energy_conumption'].max()} on {df['energy_conumption'].idxmax()}"],
-        "Minimum value in dataset": [f"{df['energy_conumption'].min()} on {df['energy_conumption'].idxmin()}"],
-    }
-    
-    daily = df.resample('D').sum()
-    
-    weekdays = df[df['dayofweek'].between(0, 4)]
-    weekends = df[df['dayofweek'].between(5, 6)]
-    sun_3am = df[(df['dayofweek'] == 6) & (df['hour'] == 3)]
-    weekdays_8_17 = weekdays[weekdays['hour'].between(8, 16)]
-    weekends_8_17 = weekends[weekends['hour'].between(8, 16)]
-    weekdays_17_8 = weekdays[~weekdays['hour'].between(8, 16)]
-    weekends_17_8 = weekends[~weekends['hour'].between(8, 16)]
-    
-    datasets = {
-        "Average and median of daily sum in dataset": daily,
-        "Average and median of daily sum on weekdays": weekdays.resample('D').sum(),
-        "Average and median of daily sum on saturdays and sundays": weekends.resample('D').sum(),
-        "Average and median of on sundays at 03:00 in the morning": sun_3am,
-        "Average daily sum between 08:00-17:00 on weekdays": weekdays_8_17.resample('D').sum(),
-        "Average daily sum between 08:00-17:00 on Saturdays and sundays": weekends_8_17.resample('D').sum(),
-        "Average daily sum between 17:00-08:00 on weekdays": weekdays_17_8.resample('D').sum(),
-        "Average daily sum between 17:00-08:00 on saturdays and sundays": weekends_17_8.resample('D').sum(),
-    }
-    
-    for k, v in datasets.items():
-        results[k]= [
-            v['energy_consumption'].mean(), 
-            v['energy_consumption'].median(),
-            v['energy_consumption'].std()
-        ]
+    max_widths = results_df.applymap(lambda x: len(str(x))).max(axis=0)
+    column_widths = max_widths.add(5)  # 5 for some padding
+    total_width = column_widths.sum() + len(column_widths) + 1  # for borders
+
+    def print_border():
+        print('-' * total_width)
+
+    def print_row(items, widths):
+        line = "|"
+        for item, width in zip(items, widths):
+            line += f" {str(item).ljust(width)} |"
+        print(line)
+
+    print_border()
+
+    # Print headers
+    headers = results_df.columns
+    print_row(headers, column_widths)
+
+    print_border()
+
+    # Print data rows
+    for _, row in results_df.iterrows():
+        values = row.values
+        print_row(values, column_widths)
+        print_border()
         
-    results_df = pd.DataFrame(results).T
-    results_df.columns = ['Value']
+def load_data(dataset_path):
+    """
+    Load energy data from a given path.
     
-    print(clr.S+'Results'+clr.E)
-    print(results_df)
+    Args:
+    - dataset_path (str): Path to the dataset.
     
-    save_path = os.path.join(os.path.dirname(dataset_path), os.path.basename(dataset_path).replace('.csv', '_results.csv'))
-    results_df.to_csv(save_path)
+    Returns:
+    - pd.DataFrame or None: loaded dataframe or None if an error occurs.
+    """
     
-    print(clr.S+f"Results saved to {save_path} !"+clr.E)
+    if os.name == 'nt':
+        command = f"type {dataset_path} | find /v /c \"\""
+    elif os.name == 'posix':
+        command = f"head -n 10 {dataset_path}"
+    else:
+        print(clr.S + "Sorry, this platform is not supported." + clr.E)
+        return None
+
+    if os.system(command) != 0:
+        print(clr.S + """Error displaying the first 10 rows of the file. 
+              Please check the file path and permissions.""" + clr.E)
+        return None
+
+    skip_rows = int(input("Enter the number of rows to skip: "))
+    try:
+        df = pd.read_csv(dataset_path, skiprows=skip_rows,
+                         sep=';', decimal=',', dayfirst=True,
+                         names=['date', 'energy_consumption'], header=0,
+                         parse_dates=['date'], date_parser=lambda x: pd.to_datetime(x, format='%d.%m.%Y %H:%M', errors='coerce'),
+                         index_col='date')
+
+        unit = input("Do you want the results in kWh or kW? ").strip().lower()
+        if unit == 'kw':
+            df['energy_consumption'] = round(df['energy_consumption'] / df.index.to_series().diff().dt.total_seconds() * 3600, 2)
+            
+        return df
+
+    except Exception as e:
+        print(clr.S + f"Error processing the dataset: {e}" + clr.E)
+        return None
+
+def compute_dataset_info(df):
+    """
+    Compute dataset statistics for different time intervals.
+
+    Args:
+        - df (pd.DataFrame): Input dataframe with energy consumption data.
+
+    Returns:
+        - pd.DataFrame: Dataframe with computed statistics.
+    """
+    
+    WEEKDAYS = df['dayofweek'].between(0, 4)
+    WEEKENDS = df['dayofweek'].between(5, 6)
+    WORK_HOURS = df['hour'].between(8, 16)
+    OFF_HOURS = ~df['hour'].between(8, 16)
+
+    datasets = {
+        "Daily sum": round(df.resample('D').sum(), 2),
+        "Weekday sum": round(df[WEEKDAYS].resample('D').sum(), 2),
+        "Weekend sum": round(df[WEEKENDS].resample('D').sum(), 2),
+        "Sunday at 03:00": round(df[(df['dayofweek'] == 6) & (df['hour'] == 3)], 2),
+        "Weekdays 08:00-17:00": round(df[WEEKDAYS & WORK_HOURS].resample('D').sum(),2),
+        "Weekends 08:00-17:00": round(df[WEEKENDS & WORK_HOURS].resample('D').sum(), 2),
+        "Weekdays 17:00-08:00": round(df[WEEKDAYS & OFF_HOURS].resample('D').sum(), 2),
+        "Weekends 17:00-08:00": round(df[WEEKENDS & OFF_HOURS].resample('D').sum(), 2),
+    }
+    
+    dataset_info = {
+        'Metric': list(datasets.keys()),
+        "Mean": [round(v['energy_consumption'].mean(),2) for v in datasets.values()],
+        "Median": [round(v['energy_consumption'].median(),2) for v in datasets.values()],
+        "Standard Deviation": [round(v['energy_consumption'].std(),2) for v in datasets.values()]
+    }
+    
+    return pd.DataFrame.from_dict(dataset_info)
+
+def compute_analysis(dataset_path):
+    """
+    Analyze energy consumption data from a given path.
+    
+    Args:
+        - dataset_path (str): Path to the dataset.
+    """
+    
+    df = load_data(dataset_path)
+    
+    print(df.head())
+
+    df = create_features(df)
+
+    basic_info = {
+     "Name of dataset": dataset_path.split('/')[-1],
+     "Time period of dataset": f"{df.index.min()} to {df.index.max()}",
+     "Count of kWh values": len(df),
+     "Total energy consumption (kWh)": round(df['energy_consumption'].sum(),2),
+     "Maximum value in dataset": df['energy_consumption'].max(),
+     "Maximum value date": df['energy_consumption'].idxmax(),
+     "Minimum value in dataset": df['energy_consumption'].min(),
+     "Minimum value date": df['energy_consumption'].idxmin(),
+    }
+
+    basic_info_df = pd.DataFrame(list(basic_info.items()), columns=['Metric', 'Value'])
+
+    dataset_info_df = compute_dataset_info(df)
+
+    print(clr.S + 'Basic Information' + clr.E)
+    basic_info_df = basic_info_df.reset_index(drop=True)
+    display_results_table(basic_info_df)
+
+    print(clr.S + 'Dataset Statistics' + clr.E)
+    display_results_table(dataset_info_df)
+
+    basic_info_path = os.path.join(os.path.dirname(dataset_path), os.path.basename(dataset_path).replace('.csv', '_basic_info.csv'))
+    dataset_info_path = os.path.join(os.path.dirname(dataset_path), os.path.basename(dataset_path).replace('.csv', '_dataset_info.csv'))
+
+    basic_info_df.to_csv(basic_info_path, index=False)
+    dataset_info_df.to_csv(dataset_info_path, index=False)
+    print(clr.S + f"Basic Info saved to {basic_info_path} !" + clr.E)
+    print(clr.S + f"Dataset Stats saved to {dataset_info_path} !" + clr.E)
 
 if __name__ == '__main__':
     
